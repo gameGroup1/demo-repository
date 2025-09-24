13d3a3c4eb17b8ab8bfdd8a933f2fbc7202f0900
DoanLoicode <198335608+LoiDoancode@users.noreply.github.com>
2025-09-24 16:51:07 +0700

    Update minh.java

--- minh.java
+++ minh.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
 public class minh {
+    public static void game(){
+        System.out.println("lodechiton");
+    }
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
         String tuong=sc.next();
         int comp=sc.nextInt();
-        if(comp==1)
-        System.out.println("tuongdglo"+"-"+tuong);
+        if(comp==1){
+            System.out.println("tuongdglo"+"-"+tuong);
+            game();
+        }
             else System.out.println("tuongkolo");
     }
 }
