0d2605006c9ff5dd7c1e905ac6bb70e456d7d6af
DoanLoicode <198335608+LoiDoancode@users.noreply.github.com>
2025-09-30 18:21:36 +0700

    fixed
    sửa wall và thêm checkCollision.java

--- Wall.java
+++ Wall.java
@@ -7,33 +7,4 @@ public class Wall {
         this.width = width;
         this.height = height;
     }
-
-    public boolean checkCollision(Ball ball) {
-        if (ball != null) {
-            int ballX = ball.x;
-            int ballY = ball.y;
-            int ballRadius = ball.radius;
-
-            if (ballX + ballRadius > x && ballX - ballRadius < x + width &&
-                ballY + ballRadius > y && ballY - ballRadius < y + height) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean checkCollision(Paddle paddle) {
-        if (paddle != null) {
-            int paddleX = paddle.x;
-            int paddleY = paddle.y;
-            int paddleWidth = paddle.width;
-            int paddleHeight = paddle.height;
-
-            if (paddleX + paddleWidth > x && paddleX < x + width &&
-                paddleY + paddleHeight > y && paddleY < y + height) {
-                return true;
-            }
-        }
-        return false;
-    }
 }
---
+++ checkCollision.java
@@ -0,0 +1,59 @@
+public class checkCollision {
+    public boolean checkBallWall(Ball ball, Wall wall) {
+        if (ball != null && wall != null) {
+            int ballX = ball.x;
+            int ballY = ball.y;
+            int ballRadius = ball.radius;
+
+            if (ballX + ballRadius > wall.x && ballX - ballRadius < wall.x + wall.width &&
+                ballY + ballRadius > wall.y && ballY - ballRadius < wall.y + wall.height) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean checkBallPaddle(Ball ball, Paddle paddle) {
+        if (ball != null && paddle != null) {
+            int ballX = ball.x;
+            int ballY = ball.y;
+            int ballRadius = ball.radius;
+
+            if (ballX + ballRadius > paddle.x && ballX - ballRadius < paddle.x + paddle.width &&
+                ballY + ballRadius > paddle.y && ballY - ballRadius < paddle.y + paddle.height) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkBallBricks(Ball ball, Bricks brick) {
+        if (ball != null && brick != null && !brick.isBreak) {
+            int ballX = ball.x;
+            int ballY = ball.y;
+            int ballRadius = ball.radius;
+
+            if (ballX + ballRadius > brick.x && ballX - ballRadius < brick.x + brick.width &&
+                ballY + ballRadius > brick.y && ballY - ballRadius < brick.y + brick.height) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean checkPaddleWall(Paddle paddle, Wall wall) {
+        if (paddle != null && wall != null) {
+            int paddleX = paddle.x;
+            int paddleY = paddle.y;
+
+            if (paddleX < wall.x) {
+                paddle.x = wall.x;
+                return true;
+            } else if (paddleX + paddle.width > wall.x + wall.width) {
+                paddle.x = wall.x + wall.width - paddle.width;
+                return true;
+            }
+        }
+        return false;
+    }
+}
